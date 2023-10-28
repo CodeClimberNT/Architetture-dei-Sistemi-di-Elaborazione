c03126627cd7b679350137e56f7b7b3aef6d2825
Nicolò Taormina <nicktaormina3@gmail.com>
2023-10-28 19:41:47 +0200

    optimized code to avoid hazard as much as possible modified pow to use shift instead of iterating mul (i was dumb)

--- lab_03/program_1_a.s
+++ lab_03/program_1_a.s
@@ -1,32 +1,5 @@
 .data
-v1: 
-.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
-.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
-.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
-.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
-.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
-.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
-.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
-.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
-v2: 
-.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
-.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
-.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
-.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
-.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
-.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
-.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
-.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
-v3: 
-.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
-.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
-.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
-.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
-.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
-.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
-.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
-.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
-v4: 
+v1:
 .double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
 .double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
 .double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
@@ -36,7 +9,37 @@ v4:
 .double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
 .double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
 
-m:  
+v2:
+.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
+.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
+.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
+.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
+.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
+.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
+.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
+.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
+
+v3:
+.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
+.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
+.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
+.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
+.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
+.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
+.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
+.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
+
+v4:
+.double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
+.double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8
+.double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8
+.double 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
+.double 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8
+.double 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8
+.double 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8
+.double 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8
+
+m:
 .word 1
 
 results:
@@ -45,84 +48,70 @@ v6: .space  512
 v7: .space  512
 
 
+
 .text
 Main:
-    daddi   R1, R0, 512     ; Initializing iterator
-    daddi   R2, R0, 63      ; Initializing iterator
-    daddi   R10, R0, 1      ; Odd/even check register
+    daddi   R1,         R0,     512             ; Initializing iterator (for memory)
+    daddi   R2,         R0,     64              ; Initializing the other iterator (for shifting and oddity)
+    daddi   R10,        R0,     1               ; Odd/even check register
+    ld      R20,        m(R0)                   ; Initialize m (R20)
+    mtc1    R0,         F11                     ; Initialize p (F11)
+    mtc1    R0,         F12                     ; Initialize k (F12)
 
-    ; Initialize m, k, p registers
-    ld      R11, m(R0)      ; Load m
-    mtc1    R0, F10         ; k
-    mtc1    R0, F11         ; p
-
-    mtc1    R0, F12         ; Prepare m result operation register
 
 Loop:
-    l.d     F1, v1(R1)      ; Load vector to FP register
-    l.d     F2, v2(R1)      ; Load vector to FP register
-    l.d     F3, v3(R1)      ; Load vector to FP register
-    l.d     F4, v4(R1)      ; Load vector to FP register
+    daddi   R1,         R1,     -8              ; Decrement iterator by 8 (assuming double elements)
+    daddi   R2,         R2,     -1              ; Decrement iterator by 1
 
-    and     R30, R2, R10    ; Use R10 as a mask to retrieve the last bit of the iterator (0 for even, 1 for odd)
-    beqz    R30, Even
+; preload for the branch
+    l.d     F1,         v1(R1)                  ; Load v1[i]
+    l.d     F4,         v4(R1)                  ; Load v4[i]
 
-Odd:
-    dmul    R12, R11, R2    ; m * i
-    mtc1    R12, F13         ; Move to FP register
-    cvt.d.l F13, F13         ; Convert to FP format F13 = m * i
-    div.d   F11, F1, F13    ; p = v1 / (m * i)
-
-    ; 2^i
-    dadd    R20, R0, R2     ; Copy iterator to R20
-    daddi   R29, R0, 2      ; Load the value of power
-    daddi   R30, R0, 1      ; Load neutral multiplication
-    j       Pow
-
-After_power:
-    cvt.l.d F30, F4          ; (int) v4
-    mfc1    R25, F4
-    ddiv    R25, R25, R30   ; v4 / 2^i
-    mtc1    R25, F10
-    cvt.d.l F10, F10         ; Convert to float
-
-    j       After_if
+    and     R30,        R2,     R10             ; Use R10 as a mask to check last bit
+    bnez    R30,        Odd                     ; if last bit is not 0 then value is Odd
 
 Even:
-    dsllv   R12, R11, R2    ; m << i
-    mtc1    R12, F13         ; Move to FP register
-    cvt.d.l F13, F13         ; Convert to FP format
-    mul.d   F11, F1, F13    ; p = v1 * m
+    dsllv   R12,        R20,    R2              ; m << i
+    mtc1    R12,        F12                     ; Convert R12 to double
+    cvt.d.l F12,        F12                     ; Convert R12 to double
+    mul.d   F11,        F1,     F12             ; p = v1[i] * (m << i)
 
-    cvt.l.d F11, F11         ; Convert FP notation to binary
-    mfc1    R11, F11         ; (int) p
+    cvt.l.d F20,        F11                     ; Convert F11 (int)p
+    mfc1    R20,        F20                     ; m = (int)p
 
     j       After_if
 
-Pow:
-    dmul    R30, R30, R29   ; Result *= 2
-    daddi   R20, R20, -1
-    bnez    R20, Pow
-    j       After_power
+Odd:
+    daddi   R12,        R0,     2               ; Initialize starting value for pow
+    cvt.l.d F4,         F4                      ; Convert v4 to int
+    dsllv   R12,        R12,    R2              ; 2^i
+    mfc1    R14,        F4                      ; Convert v4 to int
+    ddiv    R14,        R14,    R12             ; v4[i] / 2^i
+
+    mtc1    R14,        F12                     ; Move R14 to fp register (k)
+    cvt.d.l F12,        F12                     ; Convert k to fp notation
+    j       After_if
 
 After_if:
-    ; Swap multiplication and addition to hide latency
-    mul.d   F30, F11, F2     ; Multiply first
-    add.d   F5, F3, F4       ; Then add
-    s.d     F5, v5(R1)       ; Save the result in memory
+    l.d     F2,         v2(R1)                  ; Load v2[i]
+    l.d     F3,         v3(R1)                  ; Load v3[i]
 
-    ; Swap multiplication and addition to hide latency
-    mul.d   F6, F10, F1      ; Multiply first
-    div.d   F6, F5, F6       ; Then divide
-    s.d     F6, v6(R1)       ; Save the result in memory
 
-    ; Swap multiplication and addition to hide latency
-    mul.d   F7, F2, F3       ; Multiply first
-    mul.d   F7, F6, F7       ; Then multiply again
-    s.d     F7, v7(R1)       ; Save the result in memory
+    mul.d   F5,         F11,    F2              ; p * v2[i]
+    add.d   F8,         F3,     F4              ; v3[i]) + v4[i]
 
-    daddi   R1, R1, -8       ; Decrement the iterator
-    daddi   R2, R2, -1       ; Decrement the iterator
-    bnez    R1, Loop         ; Loop until the iterator reaches zero
+    add.d   F5,         F5,     F8              ; (p * v2[i]) + v3[i] + v4[i]
+    s.d     F5,         v5(R1)                  ; Store result in v5[i]
 
-    HALT                    ; The end
+    add.d   F6,         F12,    F1              ; k + v1[i]
+    div.d   F6,         F5,     F6              ; v5[i] / (k + v1[i])
+    s.d     F6,         v6(R1)                  ; Store result in v6[i]
+
+    add.d   F7,         F2,     F3              ; v2[i] + v3[i]
+    mul.d   F7,         F6,     F7              ; v6[i] * (v2[i] + v3[i])
+    s.d     F7,         v7(R1)                  ; Store result in v7[i]
+
+
+    bnez    R2,         Loop                    ; Loop until the iterator reaches 0
+
+    HALT                                        ; End of program
