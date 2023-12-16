1984053a9f93b3b08294e405274d80523594016b
Nicolò Taormina <nicktaormina3@gmail.com>
2023-12-16 10:17:59 +0100

    lab9 init

--- Lab_09/Exercise 1/irq_button.d
+++ Lab_09/Exercise 1/irq_button.d
@@ -9,3 +9,4 @@
 .\irq_button.o: C:\Users\nickt\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
 .\irq_button.o: C:\Users\nickt\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
 .\irq_button.o: button_EXINT\../led/led.h
+.\irq_button.o: button_EXINT\../timer/timer.h
